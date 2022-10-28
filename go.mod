d6816d743f663f79348f0c77d48269c453e9e911
Harsh Thakur <harsh@civo.com>
2022-10-28 07:09:51 +0530

    Add support to generate sbom, scan images
    Signed-off-by: Harsh Thakur <harsh@civo.com>

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	dagger.io/dagger v0.3.1
-	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4
+	github.com/google/uuid v1.3.0
 )
 
 require (
@@ -66,6 +66,7 @@ require (
 	go.opentelemetry.io/proto/otlp v0.18.0 // indirect
 	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
 	golang.org/x/net v0.0.0-20220811182439-13a9a731de15 // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20220722155302-e5dcc9cfc0b9 // indirect
--- main.go
+++ main.go
@@ -3,10 +3,31 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"dagger.io/dagger"
+	"github.com/google/uuid"
 )
+
+// Pipeline contains fields/clients for building a pipeline
+type Pipeline struct {
+	Client *dagger.Client
+}
+
+// NewPipeline creates a new pipeline object
+func NewPipeline() (*Pipeline, error) {
+	ctx := context.Background()
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(io.Discard))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Pipeline{
+		Client: client,
+	}, nil
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -14,22 +35,49 @@ func main() {
 		os.Exit(1)
 	}
 	repo := os.Args[1]
-	if err := build(repo); err != nil {
+	p, err := NewPipeline()
+	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
-}
-
-func build(repoURL string) error {
-	fmt.Printf("Building %s\n", repoURL)
 
 	ctx := context.Background()
-
-	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	fmt.Printf("Building image %s", repo)
+	image, err := p.Build(ctx, repo)
 	if err != nil {
-		return err
+		fmt.Println(err)
+		os.Exit(1)
 	}
-	defer client.Close()
 
+	fmt.Printf("Built image %s\n", *image)
+
+	fmt.Println("Generating SBOM for image", *image)
+	sbom, err := p.GenerateSBOM(ctx, *image)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Generated SBOM for image", *image)
+
+	fmt.Println("Scanning SBOM for vulnerabilities")
+	res, err := p.ScanVulns(ctx, *sbom)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Scanned SBOM for vulnerabilities")
+	fmt.Println(*res)
+}
+
+func getRepoName(repoURL string) string {
+	repoSplit := strings.Split(repoURL, "/")
+	return repoSplit[len(repoSplit)-1]
+}
+
+// Build builds a container image for the git repo using buildpacks
+// If build is successful, returns the image name
+func (p *Pipeline) Build(ctx context.Context, repoURL string) (*string, error) {
+	client := p.Client
 	imageTag := fmt.Sprintf("heroku/buildpacks:20")
 	packBuilder := client.Container().From(imageTag)
 
@@ -39,14 +87,50 @@ func build(repoURL string) error {
 
 	packBuilder = packBuilder.WithWorkdir("/tmp/src")
 
+	repoName := getRepoName(repoURL)
+	imageName := fmt.Sprintf("ttl.sh/%s-%s:30m", repoName, uuid.New().String()[:5])
 	build := packBuilder.Exec(dagger.ContainerExecOpts{
-		Args: []string{"/cnb/lifecycle/creator", "-app=.", "ttl.sh/random-new-image:latest"},
+		Args: []string{"/cnb/lifecycle/creator", "-app=.", imageName},
 	})
 
-	_, err = build.Stdout().Contents(ctx)
+	_, err := build.Stdout().Contents(ctx)
+	return &imageName, err
+}
+
+// GenerateSBOM generates a software bill of materials for the container image
+func (p *Pipeline) GenerateSBOM(ctx context.Context, image string) (*string, error) {
+	client := p.Client
+	bom := client.Container().From("anchore/syft:latest")
+	bom = bom.Exec(dagger.ContainerExecOpts{
+		Args: []string{image, "-o", "spdx-json"},
+	})
+
+	sbom, err := bom.Stdout().Contents(ctx)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	return &sbom, nil
+}
+
+// ScanVulns scans the SBOM for vulnerabilities
+func (p *Pipeline) ScanVulns(ctx context.Context, sbom string) (*string, error) {
+	client := p.Client
+	scanner := client.Container().From("anchore/grype:latest")
+	scanner = scanner.WithWorkdir("/tmp")
+
+	scanner = scanner.Exec(dagger.ContainerExecOpts{
+		Args: []string{"echo", sbom, ">", "/tmp/sbom.json"},
+	})
+
+	scanner = scanner.Exec(dagger.ContainerExecOpts{
+		Args: []string{"sbom:/tmp/sbom.json > /tmp/vuln.json"},
+	})
+
+	result, err := scanner.File("/tmp/vuln.json").Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
 }
